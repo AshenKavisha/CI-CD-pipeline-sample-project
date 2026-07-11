7d78a4f9f4e20d3774ff4a0e6b56abbd909780b5
ashen <kavishaashenkdh@gmail.com>
2026-07-11 10:09:05 +0530

    updated

--- cicd/src/SignIn.jsx
+++ cicd/src/SignIn.jsx
@@ -127,7 +127,7 @@ export default function SignIn() {
             variant="h4"
             sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
           >
-            Sign in
+            Sign in Oshadi
           </Typography>
           <Box
             component="form"
